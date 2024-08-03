ee309b293a8e7d203b773f7c4b149ce517db23c6
Cesar SC <csolanoc@unal.edu.co>
2024-08-02 19:09:38 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'media',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
